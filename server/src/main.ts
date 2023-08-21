0b031ba626e792a572128352abd8aca251ace57d
Raz <razaharon1993@gmail.com>
2023-08-21 15:49:07 +0300

    Spacing

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,13 +6,13 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api', {exclude: [{ path: 'health', method: RequestMethod.GET }]});
+  app.setGlobalPrefix('api', { exclude: [{ path: 'health', method: RequestMethod.GET }] });
   app.enableCors();
   app.use('/image', createProxyMiddleware({
     changeOrigin: true,
     pathRewrite: (path) => path.replace('/image/', ''),
     router: (req) => new URL(req.path.replace('/image/', '')).origin,
-}));
+  }));
   await app.listen(3000);
 }
 bootstrap();
